964c4c6b61061c254426e2ef337e34cbfe980814
Gilson Nunes Junior <gilsonjunior359@gmail.com>
2025-08-17 22:26:01 -0300

    Adicionado | Projeto | Objetos Para Interacao Com o Arquivo de Eventos

---
+++ src/main/java/library/app/FileManager.java
@@ -0,0 +1,78 @@
+package library.app;
+
+import lombok.experimental.UtilityClass;
+
+import java.io.*;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * Contém os objetos necessários para trabalhar com arquivos em geral
+ *
+ * @author gilson.junior.a1
+ */
+@UtilityClass
+public class FileManager {
+    // Atributos da classe
+    public final String FILE_PATH = Paths.get(new File("").getAbsolutePath(), "src", "main", "resources").toString();
+
+    /**
+     * Checa se um arquivo existe
+     *
+     * @param file Objeto que representa o arquivo que deve existir
+     * @return Verdadeiro se o arquivo existe, falso caso contrário
+     */
+    public boolean fileExists(final File file) {
+        return file.exists();
+    }
+
+    /**
+     * Checa se um arquivo pôde ser criado
+     *
+     * @param file Objeto que representa o arquivo que deve ser criado e conferido
+     * @return Verdadeiro se o arquivo pôde ser criado, falso caso contrário
+     * @throws IOException Caso o programa encontre problemas para interagir com o arquivo
+     */
+    public boolean createFile(final File file) throws IOException {
+        return file.createNewFile();
+    }
+
+    /**
+     * Lê e devolve o conteúdo de um arquivo
+     *
+     * @param file Objeto que representa o arquivo que deve ter o conteúdo lido e retornado
+     * @return Uma lista de String que representa o conteúdo do arquivo
+     */
+    public List<String> readFile(final File file) {
+        List<String> fileContent = new ArrayList<>();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(file.getAbsolutePath()))) {
+            String line;
+
+            while (!Objects.equals(line = reader.readLine(), null)) {
+                fileContent.add(line);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        return fileContent;
+    }
+
+    /**
+     * Escreve certo conteúdo num determinado arquivo
+     *
+     * @param file        Objeto que representa o arquivo que deve ter o conteúdo lido e retornado
+     * @param contentLine Linha que representa o conteúdo que deve ser escrito no arquivo
+     */
+    public void writeLineToFile(final File file, final String contentLine) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file.getAbsolutePath()))) {
+            writer.write(contentLine);
+            writer.newLine();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/main/java/library/event/EventType.java
@@ -0,0 +1,29 @@
+package library.event;
+
+/**
+ * Descreve os tipos disponíveis do evento
+ *
+ * @author Gilson Nunes Junior | 2025206104
+ */
+public enum EventType {
+    /**
+     * Acadêmicos / Escolares (ex.: Feira de Ciências, Sarau)
+     */
+    ACADEMIC, //
+    /**
+     * Corporativos (ex.: Feira de Emprego, Apresentação de Resultados)
+     */
+    BUSINESS, //
+    /**
+     * Entretenimento (ex.: Teatro, Show)
+     */
+    ENTERTAINMENT, //
+    /**
+     * Esportes Eletrônicos (ex. Campeonatos em LAN, Masterclasses com Times Profissionais)
+     */
+    E_SPORTS, //
+    /**
+     * Esportivos (ex.: Jogo de Futebol, Voleiball)
+     */
+    SPORTS
+}
